dbb12d9871302c3b2262ed932adb7b61071c2a3c
Mariia_Pristavko <mariia_pristavko@epam.com>
2024-06-09 11:33:20 +0500

    pet with duplicate name is added

--- src/main/java/org/springframework/samples/petclinic/web/PetController.java
+++ src/main/java/org/springframework/samples/petclinic/web/PetController.java
@@ -78,7 +78,7 @@ public class PetController {
 
     @PostMapping(value = "/pets/new")
     public String processCreationForm(Owner owner, @Valid Pet pet, BindingResult result, ModelMap model) {
-        if (StringUtils.hasLength(pet.getName()) && pet.isNew() && owner.getPet(pet.getName(), true) != null){
+        if (StringUtils.hasLength(pet.getName()) && owner.getPet(pet.getName(), true) != null){
             result.rejectValue("name", "duplicate", "already exists");
         }
         if (result.hasErrors()) {
